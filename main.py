2a0dddb9ca923e22892348ba8f6f7beed0a6287d
umer_far <farooqueumer2662@gmail.com>
2024-09-02 21:01:43 +0530

    angle calculation over

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import mediapipe as mp
 import poseEstimation as pe 
 import cv2
 poseDetector=pe.poseTrack()
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture('trainer\o1.mp4')
 
 while True:
     status,frame=cap.read()
+    frame=cv2.resize(frame,(720,360))
+    frame=poseDetector.poseDetector(frame,Draw=False)
+    lmList=poseDetector.posePosition(frame,Draw=False)
+    if len(lmList) !=0:
+      
+        rshoulder=lmList[12][1:]
+        lshoulder=lmList[11][1:]
+        lelbow,lwrist = lmList[13][1:],lmList[15][1:]
+        frame=poseDetector.findAngle(frame,11,13,15)
+    
+    
     cv2.imshow("AI_Trainer",frame)
     
     if cv2.waitKey(1) & 0xFF == ord('x'):
--- poseEstimation.py
+++ poseEstimation.py
@@ -2,7 +2,7 @@ import mediapipe as mp
 import cv2
 import numpy as np
 import time
-
+import math
 class poseTrack():
     def __init__(self,mode=False, upBody=False, smooth=True, detectionConf=0.5,trackConf=0.5):
         self.mode=mode
@@ -27,17 +27,40 @@ class poseTrack():
         return frame
     def posePosition(self,frame,Draw=True):
         
-        lmList=[]
+        self.lmList=[]
         if self.results.pose_landmarks:
             for id,lm in enumerate(self.results.pose_landmarks.landmark):
                 h,w,c=frame.shape
                 px, py = int(lm.x*w) , int(lm.y*h)
-                lmList.append([id,px,py])
+                self.lmList.append([id,px,py])
                 if Draw:
                     cv2.circle(frame,(px,py),10,(255,0,0),3)
-        return lmList
+        return self.lmList
     
-
+    def findAngle(self,frame,p1,p2,p3,draw=True):
+        
+        x1,y1 = self.lmList[p1][1:]
+        x2,y2 = self.lmList[p2][1:]
+        x3,y3 = self.lmList[p3][1:]
+        
+        angle = math.degrees(math.atan2(y3-y2, x3-x2) - math.atan2(y1-y2, x1-x2))
+        if angle < 0:
+            angle += 360
+            
+        cv2.putText(frame,text=str(int(angle)),org=(x2+10,y2+10),color=(255,0,0),fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,fontScale=2,thickness=3)
+        
+        if draw:
+            cv2.circle(frame,center=(x1,y1),radius=15,color=(255,0,255),thickness=1)
+            cv2.circle(frame,center=(x1,y1),radius=5,color=(255,0,255),thickness=-1)
+            cv2.line(frame,pt1=(x1,y1),pt2=(x2,y2),color=(255,255,0),thickness=2)
+            
+            cv2.circle(frame,center=(x2,y2),radius=15,color=(255,0,255),thickness=1)
+            cv2.circle(frame,center=(x2,y2),radius=5,color=(255,0,255),thickness=-1)
+            cv2.line(frame,pt1=(x3,y3),pt2=(x2,y2),color=(255,255,0),thickness=2)
+            
+            cv2.circle(frame,center=(x3,y3),radius=15,color=(255,0,255),thickness=1)
+            cv2.circle(frame,center=(x3,y3),radius=5,color=(255,0,255),thickness=-1)
+        return frame
 def main():
     pass
     
