9ad46bbad49e2fdbb995be32ffc62a6a1af5fb90
umer_far <farooqueumer2662@gmail.com>
2024-09-02 22:25:59 +0530

    added status bar

--- main.py
+++ main.py
@@ -20,18 +20,26 @@ while True:
 
         frame ,angle = poseDetector.findAngle(frame,11,13,15)
 
-        per=np.interp(angle, (210,310),(0,100))
-        #cv2.putText(frame,text=str(int(per)),color=(0,0,0),fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,fontScale=2,thickness=2,org=(0,350))
-
+        per = np.interp(angle, (210,310),(0,100))
+        bar = np.interp(angle,(210,310),(250,50))
+        
+        color=(255,0,0)
         #dumbel curl checking
-        if per ==100:
+        if per == 100:
+            color=(0,0,255)
             if dir == 0:
                 count+=0.5
                 dir=1
         if per == 0:
+            color=(0,0,255)
             if dir == 1:
                 count+=0.5
                 dir=0
+                
+        cv2.rectangle(frame,pt1=(30,250),pt2=(60,50),color=(0,255,0),thickness=2)
+        cv2.rectangle(frame,pt1=(30,250),pt2=(60,int(bar)),color=color,thickness=-1)
+        cv2.putText(frame,text=str(int(per)),org=(25,30),fontFace=cv2.FONT_HERSHEY_PLAIN,fontScale=2,thickness=2,color=(255,255,0))
+        
         cv2.rectangle(frame,pt1=(0,360),pt2=(100,280),color=(255,0,0),thickness=-1)
         cv2.putText(frame,org=(40,330),fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,fontScale=2,thickness=2,color=(255,255,0),text=str(int(count)))
     
